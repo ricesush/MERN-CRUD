7d5b20bbdda93b003aa7429062b55d34050784a3
ricesush <pixelartme@gmail.com>
2023-04-01 01:52:26 +0800

    completed CRUD UI, tested, status: working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,12 @@ function App() {
     title: '',
     content: '',
   });
+  const [showCreatePostForm, setShowCreatePostForm] = useState(false);
+  const [updateForm, setUpdateForm] = useState({
+    _id: null,
+    title: '',
+    content: '',
+  });
 
   // Effects
   useEffect(() => {
@@ -43,6 +49,9 @@ function App() {
     } else {
       return console.log('Cant submit empty fields');
     }
+
+    // Hiding create post form
+    setShowCreatePostForm(false);
     // clear form state
     setCreateForm({ title: '', content: '' });
   };
@@ -58,8 +67,55 @@ function App() {
     setPosts(newPosts);
   };
 
+  const updateFormFieldChangeHandler = (e) => {
+    const { name, value } = e.target;
+
+    setUpdateForm({
+      ...updateForm,
+      [name]: value,
+    });
+  };
+
+  const onUpdatePost = (post) => {
+    // Set state on update form
+    setUpdateForm({
+      title: post.title,
+      content: post.content,
+      _id: post._id,
+    });
+  };
+
+  const updatePost = async (e) => {
+    e.preventDefault();
+
+    const { title, content } = updateForm;
+    // Send the update request
+    const res = await axios.put(
+      `http://localhost:3000/posts/${updateForm._id}`,
+      {
+        title,
+        content,
+      }
+    );
+
+    const newPosts = [...posts];
+    const postIndex = posts.findIndex((post) => {
+      return post._id === updateForm._id;
+    });
+
+    newPosts[postIndex] = res.data.post;
+
+    setPosts(newPosts);
+    setUpdateForm({
+      _id: null,
+      title: '',
+      content: '',
+    });
+  };
+
   return (
     <div className='App'>
+      <button onClick={() => setShowCreatePostForm(true)}>+ Post</button>
       <section>
         <h2>Posts:</h2>
         {posts?.map((post) => {
@@ -68,31 +124,55 @@ function App() {
               <div>Title: {post.title}</div>
               <div>Content: {post.content}</div>
               <button onClick={() => deletePost(post._id)}>Delete Post</button>
+              <button onClick={() => onUpdatePost(post)}>Update Post</button>
             </div>
           );
         })}
       </section>
 
-      <section>
-        <h2>Create Note</h2>
-        <form onSubmit={createPost}>
-          <input
-            onChange={updateFormHandler}
-            value={createForm.title}
-            type='text'
-            name='title'
-          />
-          <textarea
-            onChange={updateFormHandler}
-            value={createForm.content}
-            name='content'
-            id=''
-            cols='30'
-            rows='10'
-          ></textarea>
-          <button type='submit'>Submit</button>
-        </form>
-      </section>
+      {updateForm._id && (
+        <section>
+          <h2>Update Post</h2>
+          <form onSubmit={updatePost}>
+            <input
+              onChange={updateFormFieldChangeHandler}
+              value={updateForm.title}
+              name='title'
+            />
+            <textarea
+              onChange={updateFormFieldChangeHandler}
+              value={updateForm.content}
+              name='content'
+              cols='30'
+              rows='10'
+            />
+            <button type='submit'>Save</button>
+          </form>
+        </section>
+      )}
+
+      {showCreatePostForm && (
+        <section>
+          <h2>Create Note</h2>
+          <form onSubmit={createPost}>
+            <input
+              onChange={updateFormHandler}
+              value={createForm.title}
+              type='text'
+              name='title'
+            />
+            <textarea
+              onChange={updateFormHandler}
+              value={createForm.content}
+              name='content'
+              id=''
+              cols='30'
+              rows='10'
+            ></textarea>
+            <button type='submit'>Submit</button>
+          </form>
+        </section>
+      )}
     </div>
   );
 }
