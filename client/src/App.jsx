c5d2149fa465a5b63f66dcccce798d714dd6e372
ricesush <pixelartme@gmail.com>
2023-03-31 22:54:43 +0800

    defined state with null, fetch posts with axios and set fetched data as new state. Created UI for Title and Content of each fetched posts.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [posts, setPosts] = useState(null);
 
-  return <div className='App'>Hello World</div>;
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    // Fetch the posts
+    const res = await axios.get('http://localhost:3000/posts');
+    setPosts(res.data.posts);
+    // Set to state
+    console.log(res);
+  };
+
+  return (
+    <div className='App'>
+      <section>
+        <h2>Posts:</h2>
+        {posts.map((post) => {
+          return (
+            <>
+              <div>Title: {post.title}</div>
+              <div>Content: {post.content}</div>
+            </>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
