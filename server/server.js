84ed730f4bcb2bcfd04f304e9a8023e5339b0b88
ricesush <pixelartme@gmail.com>
2023-04-13 22:20:07 +0800

    create postCreate function. tested and working

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { ConnectToDb } from './config/connectToDb.js';
+import { postRoute } from './src/routes/Posts.js';
 
 // importing ENV variables
 dotenv.config();
@@ -12,10 +13,15 @@ ConnectToDb();
 // Creating application level middleware
 const app = express();
 
+// Converting API request to JSON
+app.use(express.json());
+
 // Routing
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
+
+app.use('/posts', postRoute);
 
 // Starting the server
 
