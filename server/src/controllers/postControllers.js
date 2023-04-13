4942f2025caa547851d1f06abfbbfe1611fef274
ricesush <pixelartme@gmail.com>
2023-04-13 22:09:06 +0800

    created the controllers

---
+++ server/src/controllers/postControllers.js
@@ -0,0 +1,18 @@
+// Importing dependecies
+import Post from '../models/post';
+
+// Defining the controllers
+
+export const createPost = async (req, res) => {
+  // getting data from the req body
+  const { author, content } = req.body;
+
+  //   creating the post
+  const post = await Post.create({
+    author: author,
+    content: content,
+  });
+
+  //  Responding with the created caht
+  res.json({ post });
+};
