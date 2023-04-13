f7fa10309ebca87fd840b953ff86440eb2953294
ricesush <pixelartme@gmail.com>
2023-04-13 21:45:14 +0800

    modified the server.js. tested and running on port 3000

--- server/server.js
+++ server/server.js
@@ -1,1 +1,20 @@
+// Importing dependecies
+import express from 'express';
+import dotenv from 'dotenv';
 
+// importing ENV variables
+dotenv.config();
+
+// Creating application level middleware
+const app = express();
+
+// Routing
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+// Starting the server
+
+app.listen(process.env.PORT, () =>
+  console.log(`Running on Port ${process.env.PORT}`)
+);
