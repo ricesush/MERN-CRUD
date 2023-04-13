25253c1f9050da47e0efa057a7ce7c3e0665dbae
ricesush <pixelartme@gmail.com>
2023-04-13 22:29:22 +0800

    created route for fetching the posts

--- server/src/controllers/postControllers.js
+++ server/src/controllers/postControllers.js
@@ -3,6 +3,14 @@ import Post from '../models/post.js';
 
 // Defining the controllers
 
+export const fetchPosts = async (req, res) => {
+  // fetching the posts
+  const post = await Post.find();
+
+  //   responding in JSOn with all the posts
+  res.json({ post });
+};
+
 export const createPost = async (req, res) => {
   // getting data from the req body
   const { author, content } = req.body;
--- server/src/routes/Posts.js
+++ server/src/routes/Posts.js
@@ -1,12 +1,12 @@
 // Importing dependecies
-import { createPost } from '../controllers/postControllers.js';
+import { createPost, fetchPosts } from '../controllers/postControllers.js';
 import express from 'express';
 
 // creating router level middleware
 const router = express.Router();
 
 // Defining the routes
-router.route('/').post(createPost);
+router.route('/').post(createPost).get(fetchPosts);
 
 // exporting routes
 export { router as postRoute };
