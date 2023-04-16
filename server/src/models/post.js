1bb506c7204bc8ad36f73219970687f57bc90574
ricesush <pixelartme@gmail.com>
2023-04-16 08:37:11 +0800

    created the model with the timestamp

---
+++ server/src/models/post.js
@@ -0,0 +1,18 @@
+// Importing dependecies
+import mongoose from 'mongoose';
+
+// Definging the schema
+const postSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamps: true,
+});
+
+// creating the model
+export const Post = mongoose.model('Post', postSchema);
