841641f7e873fb5f6be0a870706aa1f62ffe9d92
ricesush <pixelartme@gmail.com>
2023-04-16 11:04:33 +0800

    created createfPost function, tested and working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 function App() {
   const [posts, setPosts] = useState(null);
+  const [createPostForm, setCreatePostForm] = useState({
+    author: '',
+    content: '',
+  });
 
   useEffect(() => {
     fetchPosts();
@@ -16,6 +20,28 @@ function App() {
     setPosts(res.data.posts);
   };
 
+  const createPost = async (e) => {
+    e.preventDefault();
+    // creating the post
+    const post = await axios.post(
+      'http://localhost:3000/posts',
+      createPostForm
+    );
+
+    fetchPosts();
+  };
+
+  const createPostHandler = async (e) => {
+    // destructuring the data
+    const { name, value } = e.target;
+
+    // updating the state createPostForm
+    setCreatePostForm({
+      ...createPostForm,
+      [name]: value,
+    });
+  };
+
   return (
     <div className='App'>
       {posts?.map((post) => {
@@ -27,6 +53,28 @@ function App() {
           </div>
         );
       })}
+      <br />
+      <form onSubmit={createPost}>
+        <label htmlFor='author'>Author: </label>
+        <input
+          type='text'
+          id='author'
+          name='author'
+          onChange={createPostHandler}
+          value={createPostForm.author}
+        />
+        <br />
+        <textarea
+          name='content'
+          id='content'
+          cols='30'
+          rows='3'
+          onChange={createPostHandler}
+          value={createPostForm.content}
+        ></textarea>
+        <br />
+        <button type='submit'>Post</button>
+      </form>
     </div>
   );
 }
