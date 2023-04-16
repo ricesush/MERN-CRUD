69c758f66535f22de294b8dee62964c461f01595
ricesush <pixelartme@gmail.com>
2023-04-16 10:53:19 +0800

    created function on client for fetching all the posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    // fetching all the posts
+    const res = await axios.get('http://localhost:3000/posts');
+
+    console.log(res.data.posts);
+    setPosts(res.data.posts);
+  };
+
+  return (
+    <div className='App'>
+      {posts?.map((post) => {
+        return (
+          <div key={post._id}>
+            <div>Author: {post.author}</div>
+            <div>{post.createdAt}</div>
+            <div>{post.content}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
