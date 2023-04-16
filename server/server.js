9b44c887404711a2bf97ceefa62bb7a66d4e70ac
ricesush <pixelartme@gmail.com>
2023-04-16 08:22:13 +0800

    installed environment and set up the server. tested and working

--- server/server.js
+++ server/server.js
@@ -1,30 +1,19 @@
 // Importing dependecies
 import express from 'express';
 import dotenv from 'dotenv';
-import { ConnectToDb } from './config/connectToDb.js';
-import { postRoute } from './src/routes/Posts.js';
 
-// importing ENV variables
+// Importing ENV variables
 dotenv.config();
-
-// Starting the Connection to Db
-ConnectToDb();
 
 // Creating application level middleware
 const app = express();
-
-// Converting API request to JSON
-app.use(express.json());
 
 // Routing
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
 
-app.use('/posts', postRoute);
-
-// Starting the server
-
+// Runing the server
 app.listen(process.env.PORT, () =>
-  console.log(`Running on Port ${process.env.PORT}`)
+  console.log(`Running on PORT: ${process.env.PORT}`)
 );
