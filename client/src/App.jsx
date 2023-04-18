66c473561dcf5d334b67375d07a8bce1a3f6599c
ricesush <pixelartme@gmail.com>
2023-04-18 13:35:38 +0800

    creeated fetch post fn. tested and working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const API = 'http://localhost:3000/posts';
 
-  return <div className='App'>Hello</div>;
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const res = await axios.get(API);
+
+    setPosts(res.data.posts);
+    console.log(posts);
+  };
+
+  return (
+    <div className='App'>
+      {posts?.map((post) => {
+        return (
+          <div key={post._id}>
+            <div>{post.author}</div>
+            <div>{post.content}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
