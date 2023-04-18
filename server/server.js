5b667413dcdc5f8bebef5d4b164ffe02559c6dc8
ricesush <pixelartme@gmail.com>
2023-04-18 11:48:17 +0800

    defined connection to mongoDb. tested and working

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 // Importing dependecies
 import express from 'express';
 import dotenv from 'dotenv';
+import { ConnectToDb } from './config/connectToDb.js';
 
 // importing env variables
 dotenv.config();
+
+ConnectToDb();
 
 // creating application level middleware
 const app = express();
@@ -16,5 +19,5 @@ app.get('/', (req, res) => {
 // Starting the server
 
 app.listen(process.env.PORT, () =>
-  console.log(`Server started at port: ${process.env.PORT} `)
+  console.log(`Server started at http://localhost:${process.env.PORT}/  `)
 );
