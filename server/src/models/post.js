bae09ffa2648822863160909afac25b771febbac
ricesush <pixelartme@gmail.com>
2023-04-26 00:13:48 +0800

    created the model

---
+++ server/src/models/post.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 20,
+    },
+    title: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 20,
+    },
+    author: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 50,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model('Post', postSchema);
