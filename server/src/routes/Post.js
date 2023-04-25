ed7973f8607a710633100a3d501a66e9f41be699
ricesush <pixelartme@gmail.com>
2023-04-25 23:38:41 +0800

    started all voer again

--- server/src/routes/Post.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createPost } from '../controllers/postController.js';
-
-const router = express.Router();
-
-router.route('/').post(createPost);
-
-export { router as postRoute };
