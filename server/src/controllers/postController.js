61090c1a1a5a31e2a2a83b44106fc94f54dd6d83
ricesush <pixelartme@gmail.com>
2023-04-26 03:49:01 +0800

    created upadte fn. tested and working

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -29,3 +29,38 @@ export const createPost = async (req, res) => {
     console.log(error);
   }
 };
+
+export const singlePost = async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    const post = await Post.findById(postId);
+    res.json(post);
+  } catch (error) {
+    res.json({ err: "Didn't find that post" });
+    console.log(error);
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+
+  const { author, title, content } = req.body;
+
+  if (!author || !title || !content) {
+    return res.json({ msg: 'All fiields are required!' });
+  }
+
+  try {
+    const post = await Post.findByIdAndUpdate(postId, {
+      author,
+      title,
+      content,
+    });
+
+    res.json({ msg: 'Succesfully updated the post!' });
+  } catch (error) {
+    res.json({ err: 'Failed to update the post!' });
+    console.log({ error });
+  }
+};
--- server/src/routes/Post.js
+++ server/src/routes/Post.js
@@ -1,8 +1,14 @@
-import { allPosts, createPost } from '../controllers/postController.js';
+import {
+  allPosts,
+  createPost,
+  singlePost,
+  updatePost,
+} from '../controllers/postController.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.route('/').post(createPost).get(allPosts);
+router.route('/:id').get(singlePost).put(updatePost);
 
 export { router as postRouter };
