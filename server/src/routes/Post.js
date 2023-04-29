067147a5afdf03bec157d7aaceb6572221ae34c5
ricesush <pixelartme@gmail.com>
2023-04-29 22:03:34 +0800

    created routes

---
+++ server/src/routes/Post.js
@@ -0,0 +1,8 @@
+import { createPost } from '../controllers/postController.js';
+import express from 'express';
+
+const router = express.Router();
+
+router.route('/').post(createPost);
+
+export { router as postRouter };
